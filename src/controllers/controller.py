2a1b00e87dcb12085f5490776c84396ce8a8914a
Jefferson de Andrade Santos <jeffersonsantos@lnxcit020076.cit>
2019-04-01 16:03:22 -0300

    Get real postgres data

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -2,17 +2,21 @@ from views import render
 import models
 
 async def index(request):
-    data = {}
-    users = await models.objects.get(models.User.select().dicts())
-
+    data = []
+    users = await models.objects.execute(models.User.select().dicts())
+    print(users)
     if users is None:
         user0 = await models.objects.create(models.User, username='test1', password='123', email='test1@test.com')
         user1 = await models.objects.create(models.User, username='test2', password='123', email='test2@test.com')
         user2 = await models.objects.create(models.User, username='test3', password='123', email='test3@test.com')
-        users = await models.objects.get(models.User.select().dicts())
-        print(users)
+        users = await models.objects.execute(models.User.select().dicts())
 
-    print(users)
+    for user in users:
+        u={}
+        u['username'] = user['username']
+        u['email'] = user['email']
+        data.append(u)
+        
     status = 200
     return await render.json(data, status)
 
