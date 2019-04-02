d2e4084270f3279d95c75f7ee0566e1133cf5311
jefferson.santos <jeffersonsantos@ciandt.com>
2019-04-02 03:56:00 +0000

    Create dummy users by range

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -13,9 +13,8 @@ async def users(request):
         count_users = await models.objects.count(models.User.select())
         users = await models.objects.execute(models.User.select().dicts())
         if count_users == 0:
-            user0 = await models.objects.create(models.User, username='test1', password='123', email='test1@test.com')
-            user1 = await models.objects.create(models.User, username='test2', password='123', email='test2@test.com')
-            user2 = await models.objects.create(models.User, username='test3', password='123', email='test3@test.com')
+            for x in range(0, 5):
+                await models.objects.create(models.User, username=f'test{x}', password='123', email=f'test{x}@test.com')
             users = await models.objects.execute(models.User.select().dicts())
 
         for user in users:
